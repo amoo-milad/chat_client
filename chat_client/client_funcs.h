bc27e3bbf399515b2a870aa2ee509140ecc2a5e7
amoo-milad <30766645+amoo-milad@users.noreply.github.com>
2017-10-15 13:34:34 +0330

    added two  non-versioned files: client_funcs.cpp and client_funcs.h

---
+++ chat_client/client_funcs.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+#ifndef _CLIENT_FUNCS_H
+#define _CLIENT_FUNCS_H
+
+#define DEFAULT_BUFLEN 1024
+#define DEFAULT_PORT "15000"
+
+
+
+#endif // !_CLIENT_FUNCS_H
+
